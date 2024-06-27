34710093ea3856a5b3b585b23d5f3738bd543b9a
toris <ironjustlikethat@gmail.com>
2024-06-27 18:44:06 +0900

    ✨ [Feat] : 메인페이지 배너 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import Banner from '@/components/Carousel/Banner';
 import { CategoryResume } from '@/components/Resume';
 import { getResumes } from '@/utils/fetch';
 
 export default async function Home() {
   const resumes = await getResumes({});
+
   return (
-    <main className="mt-8 flex min-h-screen flex-1 flex-col items-center text-clip">
-      {/* <Banner images={images} /> */}
+    <main className="mt-8 flex min-h-screen flex-1 flex-col items-center gap-20 text-clip">
+      <Banner />
       <CategoryResume {...resumes} />
     </main>
   );
