e6b94479493c92ca04dcd3846272faddc4a2b695
toris <ironjustlikethat@gmail.com>
2024-07-08 09:58:34 +0900

    ♻️ [Chore] : lottie 의존성 설치와 eslint 규칙추가, next-auth type 변경

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -5,6 +5,7 @@ declare module 'next-auth' {
   interface Session {
     user: {
       accessToken: string;
+      refreshToken: string;
       name?: string;
       email?: string;
       image?: string;
