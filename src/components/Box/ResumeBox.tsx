43d92ef6cea27e36b606e77dd367d34d42cbb91d
toris <ironjustlikethat@gmail.com>
2024-07-09 00:03:07 +0900

    🐛 [Fix] : Image 컴포넌트 영억 벗어나는 에러 해결

--- src/components/Box/ResumeBox.tsx
+++ src/components/Box/ResumeBox.tsx
@@ -25,14 +25,18 @@ const ResumeBox: FC<ResumeBoxProps> = ({
       className="h-80 rounded-lg bg-white shadow-md hover:bg-slate-100 hover:shadow-2xl sm:w-40 md:w-72 lg:w-72"
     >
       <div className="mb-3 flex h-full flex-col items-center justify-between p-3">
-        <Image
-          src={thumbnail}
-          alt="thumbnail"
-          width={260}
-          height={220}
-          placeholder="blur"
-          blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg=="
-        />
+        <div className="relative h-[220px] w-full">
+          {/* 부모 요소에 상대적 위치 및 고정된 높이 설정 */}
+          <Image
+            src={thumbnail}
+            alt="thumbnail"
+            layout="fill" // 부모 요소를 채우도록 설정
+            objectFit="cover" // 이미지가 부모 요소를 덮도록 설정
+            placeholder="blur"
+            blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg=="
+            className="rounded-t-lg" // 이미지 상단 모서리를 둥글게 설정
+          />
+        </div>
         <h3 className="mt-2 self-start text-lg font-semibold">{title}</h3>
         <div className="my-3 flex w-full flex-row justify-between text-sm">
           <p>{userId}</p>
