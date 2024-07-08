2a0fdd36d9175a7b4ffe781c205aeda5958affda
toris <ironjustlikethat@gmail.com>
2024-07-08 18:18:00 +0900

    ✨ [Feat] : aws s3 이미지, PDF 다운로드 API 추가

---
+++ src/app/api/download/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest } from 'next/server';
+export interface FileResponse {
+  type: string;
+  arrayBuffer: number[];
+}
+export async function GET(req: NextRequest) {
+  console.log(req.nextUrl.searchParams.get('url'));
+  const url = req.nextUrl.searchParams.get('url');
+  // 파일 경로에 요청
+  const rawData = await fetch(url as string);
+
+  const blob = await rawData.blob();
+
+  // 브라우저에서 Blob 데이터를 만들기 위한 정보 응답
+  return Response.json({
+    type: blob.type,
+    arrayBuffer: Object.values(new Uint8Array(await blob.arrayBuffer()))
+  });
+}
