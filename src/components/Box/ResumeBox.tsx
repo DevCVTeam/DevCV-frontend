c56dce952c371f64d23aacdd3ba2a6f0b43e3f6e
toris <ironjustlikethat@gmail.com>
2024-11-27 23:18:40 +0900

    Chore: 윈도우 프로그램 생성

--- src/components/Box/ResumeBox.tsx
+++ src/components/Box/ResumeBox.tsx
@@ -24,21 +24,20 @@ const ResumeBox: FC<ResumeBoxProps> = ({
       href={`/resume/${resumeId}`}
       className="3xl:w-72 size-full rounded-3xl bg-white shadow-md hover:bg-slate-100 hover:shadow-2xl "
     >
-      <div className="mb-3 flex h-full flex-col items-center justify-between p-3">
-        <div className="relative h-[220px] w-full">
+      <div className="mb-3 flex h-full flex-col items-center justify-around p-3 gap-2">
+        <div className="relative h-[180px] w-full">
           {/* 부모 요소에 상대적 위치 및 고정된 높이 설정 */}
           <Image
             src={thumbnail}
             alt="thumbnail"
             layout="fill" // 부모 요소를 채우도록 설정
-            objectFit="cover" // 이미지가 부모 요소를 덮도록 설정
+            objectFit="contain" // 이미지가 부모 요소를 덮도록 설정
             placeholder="blur"
             blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg=="
-            className="rounded-t-lg" // 이미지 상단 모서리를 둥글게 설정
           />
         </div>
-        <h3 className="mt-2 self-start font-semibold">{title}</h3>
-        <div className="my-3 flex w-full flex-row justify-between text-sm">
+        <h3 className="self-start font-semibold">{title}</h3>
+        <div className="flex w-full flex-row justify-between text-sm">
           <p>{userId}</p>
           <p className="font-bold text-black">
             가격: {price.toLocaleString()}원
