98672f69cabc4b8a1cc2e3282f528e11aa4e1b62
toris <ironjustlikethat@gmail.com>
2025-02-19 03:37:20 +0900

    ✨ [Feat] : GA 추가

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -4,6 +4,7 @@ import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { SessionProvider } from 'next-auth/react';
+import Head from 'next/head';
 import Script from 'next/script';
 import { Suspense } from 'react';
 import Footer from './Footer/Footer';
@@ -41,25 +42,27 @@ export const NextLayout = ({ children }: Props) => {
   return (
     <div className="box-border max-h-screen min-h-screen w-full">
       <div className="px-4 flex flex-1 flex-col sm:px-8 md:px-16 lg:px-32 xl:px-32">
-        <Script src="https://cdn.iamport.kr/v1/iamport.js" />
-        {/* Google Analytics */}
-        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS && (
-          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
-        )}
+        <Head>
+          <Script src="https://cdn.iamport.kr/v1/iamport.js" />
+          {/* Google Analytics */}
+          {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS && (
+            <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+          )}
 
-        {/* Google Tag Manager */}
-        {process.env.NEXT_PUBLIC_GOOGLE_TAG && (
-          <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG} />
-        )}
+          {/* Google Tag Manager */}
+          {process.env.NEXT_PUBLIC_GOOGLE_TAG && (
+            <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG} />
+          )}
 
-        {/* Google AdSense */}
-        {process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_CLIENT_ID && (
-          <Script
-            async
-            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_CLIENT_ID}`}
-            crossOrigin="anonymous"
-          ></Script>
-        )}
+          {/* Google AdSense */}
+          {process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_CLIENT_ID && (
+            <Script
+              async
+              src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_CLIENT_ID}`}
+              crossOrigin="anonymous"
+            ></Script>
+          )}
+        </Head>
 
         <Header />
         <main className="mt-20 flex flex-1 flex-col ">{children}</main>
